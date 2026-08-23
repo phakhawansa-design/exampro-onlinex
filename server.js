b34b59c49db8de512d897ec6ca167f7dd1d9e253
phakhawansa-design <phakhawan.sa@rmuti.ac.th>
2026-08-23 17:20:41 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static(__dirname));
 
 const { Pool } = require('pg');
